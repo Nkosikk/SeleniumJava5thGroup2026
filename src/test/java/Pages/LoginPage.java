f29a152c82c1d175d94f8a07e42663b1f41e2395
Aphiwe Mncedisi <aphiwe@Aphiwes-MacBook-Air.local>
2026-08-26 19:44:39 +0200

    deleted extentReport.

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -23,6 +23,7 @@ public class LoginPage {
 
     public void enterUsername(String username) {
         usernameField.sendKeys(username);
+
     }
 
     // ToDo - Implement methods clicking the login button
